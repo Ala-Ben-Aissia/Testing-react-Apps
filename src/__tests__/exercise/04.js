fdd8831cbc83ea7562dd29c4f8fa2558abffd89a
Ala Ben Aissia <xalabenaissiax@outlook.com>
2024-07-22 16:12:45 +0100

    Testing Form extra-credits 1..3

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -4,20 +4,27 @@
 const {screen, render} = require('@testing-library/react')
 const {default: userEvent} = require('@testing-library/user-event')
 const {default: Login} = require('components/login')
+import faker from 'faker'
 
 test('submitting the form calls onSubmit with username and password', async () => {
-  let submittedData
-  const handleSubmit = data => (submittedData = data)
+  function buildLoginForm({password}) {
+    return {
+      username: faker.internet.userName(),
+      password: password ?? faker.internet.password(),
+    }
+  }
+  const onSubmit = jest.fn()
+  render(<Login onSubmit={onSubmit} />)
 
-  render(<Login onSubmit={handleSubmit} />)
-  const username = screen.getByRole('textbox', {name: /username/i})
-  const password = screen.getByLabelText(/password/i)
+  const {username, password} = buildLoginForm({password: 'abc'})
+  const usernameEl = screen.getByRole('textbox', {name: /username/i})
+  const passwordEl = screen.getByLabelText(/password/i)
 
-  await userEvent.type(username, 'Ala')
-  await userEvent.type(password, '0000')
+  await userEvent.type(usernameEl, username)
+  await userEvent.type(passwordEl, password)
   await userEvent.click(screen.getByRole('button', {name: /submit/i}))
 
-  expect(submittedData).toEqual({username: 'Ala', password: '0000'})
+  expect(onSubmit).toHaveBeenCalledWith({username, password: 'abc'})
 })
 
 /*
